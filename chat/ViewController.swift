573e17f50d5e12146e281fd8c667e0c65e751d6f
Vlad Suchkov <vsuchkov96@gmail.com>
2021-04-02 15:24:24 +0400

    initial commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by vlsuv on 02.04.2021.
+//  Copyright © 2021 vlsuv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
